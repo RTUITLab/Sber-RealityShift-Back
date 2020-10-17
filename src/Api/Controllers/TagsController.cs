0f917ee374c3e55dff70eb4b3bd370703c2f7b2a
Maksim Makushchenko <maksalmak@gmail.com>
2020-10-17 22:46:20 +0300

    Tags controller added

---
+++ src/Api/Controllers/TagsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Database;
+using Models.Links;
+
+namespace Api.Controllers
+{
+    [Route("api/tags")]
+    [ApiController]
+    public class TagsController : ControllerBase
+    {
+        private readonly SberDbContext _context;
+
+        public TagsController(SberDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> GetGeneralInfoTags(string match = "")
+        {
+            return await _context.GeneralInfoTags
+                .Where(t => t.Tag.ToUpper().Contains(match.ToUpper()))
+                .Select(t => t.Tag)
+                .ToListAsync();
+        }
+    }
+}

c36bb61615fde3fc89cc904d2f1e0e6a050547b0
Maksim Makushchenko <maksalmak@gmail.com>
2020-10-18 04:30:06 +0300

    Add courses action

---
+++ src/Api/Controllers/CoursesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly SberDbContext dbContext;
+
+        public CoursesController(SberDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<string>>> Get()
+        {
+            return await dbContext
+                .Modules
+                .Select(m => m.Course)
+                .Where(c => c != null)
+                .Distinct()
+                .ToListAsync();
+        }
+    }
+}
